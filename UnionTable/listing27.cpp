0d35c157c5f70a6834d7eaf304f6213b6ad957d8
Douglas Leeder <douglas.leeder@gmail.com>
2024-07-08 21:29:12 +0100

    Make internal bits static.

--- UnionTable/listing27.cpp
+++ UnionTable/listing27.cpp
@@ -4,8 +4,8 @@
 
 #include "../Union/shape_union.h"
 
-f32 const CTable[Shape_Count] = {1.0f, 1.0f, 0.5f, Pi32};
-f32 GetAreaUnion(shape_union Shape)
+static f32 const CTable[Shape_Count] = {1.0f, 1.0f, 0.5f, Pi32};
+static f32 GetAreaUnion(shape_union Shape)
 {
     f32 Result = CTable[Shape.Type]*Shape.Width*Shape.Height;
     return Result;

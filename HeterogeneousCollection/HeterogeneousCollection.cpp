e94e2ae0b4a044941697c4b4e190c2a89de6bab9
Douglas Leeder <douglas.leeder@gmail.com>
2024-08-03 13:16:25 +0100

    Move functions to cpp and tidy formatting.

--- HeterogeneousCollection/HeterogeneousCollection.cpp
+++ HeterogeneousCollection/HeterogeneousCollection.cpp
@@ -1,3 +1,46 @@
 
 #include "HeterogeneousCollection.h"
 
+using namespace andyg;
+
+heterogeneous_container::heterogeneous_container(const heterogeneous_container& _other)
+{
+    *this = _other;
+}
+
+heterogeneous_container::~heterogeneous_container()
+{
+    clear();
+}
+
+heterogeneous_container& heterogeneous_container::operator=(const heterogeneous_container& _other)
+{
+    clear();
+    clear_functions = _other.clear_functions;
+    copy_functions = _other.copy_functions;
+    size_functions = _other.size_functions;
+    for (auto&& copy_function : copy_functions)
+    {
+        copy_function(_other, *this);
+    }
+    return *this;
+}
+
+void heterogeneous_container::clear()
+{
+    for (auto&& clear_func : clear_functions)
+    {
+        clear_func(*this);
+    }
+}
+
+size_t heterogeneous_container::size() const
+{
+    size_t sum = 0;
+    for (auto&& size_func : size_functions)
+    {
+        sum += size_func(*this);
+    }
+    // gotta be careful about this overflowing
+    return sum;
+}
